297f536d32713dc434bd10461cd0e98ed8dbda3e
Simon Bösch <simon-andreas.boesch@hotmail.com>
2020-05-29 09:43:00 +0200

    Use css variables with tailwind

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
         fontFamily: {
             'heading': ['Pacifico', 'cursive'],
             'body': ['Quicksand', 'sans-serif']
+        },
+        colors: {
+            primary: {
+                d: "var(--color-primary-darker)",
+                0: "var(--color-primary)",
+                l: "var(--color-primary-lighter)"
+            },
+            secondary: {
+                d: "var(--color-secondary-darker)",
+                0: "var(--color-secondary)",
+                l: "var(--color-secondary-lighter)"
+            }
         }
     },
     variants: {
