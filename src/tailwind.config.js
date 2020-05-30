537fd66f7b2914888dbde4cc5cbefbb4f47ed62e
Simon Bösch <simon-andreas.boesch@hotmail.com>
2020-05-30 11:29:19 +0200

    Solve specifity issues

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,19 +1,22 @@
 module.exports = {
+    important: '#app',
     theme: {
-        fontFamily: {
-            'heading': ['Pacifico', 'cursive'],
-            'body': ['Quicksand', 'sans-serif']
-        },
-        colors: {
-            primary: {
-                d: "var(--color-primary-darker)",
-                0: "var(--color-primary)",
-                l: "var(--color-primary-lighter)"
+        extend: {
+            fontFamily: {
+                'heading': ['Pacifico', 'cursive'],
+                'body': ['Quicksand', 'sans-serif']
             },
-            secondary: {
-                d: "var(--color-secondary-darker)",
-                0: "var(--color-secondary)",
-                l: "var(--color-secondary-lighter)"
+            colors: {
+                primary: {
+                    d: "var(--color-primary-darker)",
+                    0: "var(--color-primary)",
+                    l: "var(--color-primary-lighter)"
+                },
+                secondary: {
+                    d: "var(--color-secondary-darker)",
+                    0: "var(--color-secondary)",
+                    l: "var(--color-secondary-lighter)"
+                }
             }
         }
     },
